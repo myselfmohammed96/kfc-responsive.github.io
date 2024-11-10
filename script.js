7caa483c7482e89739316e6d21baa423b37fecd2
myselfmohammed96 <myselfmohammed96@gmail.com>
2024-11-11 02:53:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.Ham-icon')
+const menuList = document.querySelector('.HB-menu')
+menu.addEventListener('click',()=>{
+    menuList.classList.toggle('showmenu')
+})
+
